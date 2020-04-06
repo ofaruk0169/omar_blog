02d586ce1470c2d8d18ab82693401ca666bf3fb8
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-04-06 19:23:22 +0100

    css spacing added

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @Section('content')
-       <h1>{{$title}} We are currently on time - 3:06:20</h1>
+       <h1>{{$title}} </h1>
        <p>My name is Omar Faruk</p>
+
+      
 @endsection
+
