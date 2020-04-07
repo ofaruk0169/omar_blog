480481783012bac0b0d283402e6a67d5aef2189c
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-04-07 11:28:59 +0100

    finished blog

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,9 +9,9 @@
           <section class="page-section bg-primary" id="about">
               <div class="container">
                   <div class="row justify-content-center">
-                      <div class="col-lg-8 text-center">
-                          <h2 class="text-white mt-4">Hey, welcome to my blog.</h2>
-                          <hr class="divider light my-4" />
+                      <div class="col-lg-10 text-center">
+                          <h2 class="text-white mt-0 " id="heyscreen">Hey, Welcome to My Blog.</h2>
+                          <hr class="divider light mb-4" />
                           <p class="text-white-50 mb-4">I don't really have a plan for this blog. 
                                  Honestly, I just created this to test my skills
                                  with the Laravel framework, but it could also be a place 
@@ -21,11 +21,13 @@
                       </div>
                   </div>
               </div>
+              
           </section>
           <!-- Services section-->
           <section class="page-section" id="services">
               <div class="container">
-                  <h2 class="text-center mt-0">What I love</h2>
+                     <img src="/img/me.jpg" alt="this is a picture of me" height="100%" width="100%" class="homeimg">
+                  <h2 class="text-center mt-5">What I love</h2>
                   <hr class="divider my-8" />
                   <div class="row">
                       <div class="col-lg-3 col-md-6 text-center">
@@ -62,30 +64,39 @@
          
          
           <!-- Call to action section-->
-          <section class="page-section bg-primary text-white">
+          {{-- <section class="page-section bg-primary text-white">
               <div class="container text-center">
-                  <h2 class="mb-4">Free Download at Start Bootstrap!</h2>
+                  <h2 class="mb-4">dd  
+                     <div class="social-links">
+                            <a href="#" class="twitter"><i class="fa fa-github"></i></a>
+                            <a href="https://www.messenger.com/t/osmosmar" class="facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="https://open.spotify.com/user/3cvol9wlqnblmxj0xv56crj8a" class="instagram"><i class="fa  fa-spotify"></i></a>
+                            <a href="#" class="google-plus"><i class="fa fa-facebook-messenger "></i></a>
+                            <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+                          </div>
+                  </h2>
                   
               </div>
-          </section>
+          </section> --}}
           <!-- Contact section-->
           <section class="page-section" id="contact">
               <div class="container">
+                     <img src="/img/space.jpg" alt="this is a cool picture i found" height="100%" width="100%" class="homeimg">
                   <div class="row justify-content-center">
                       <div class="col-lg-8 text-center">
-                          <h2 class="mt-0">Let's Get In Touch!</h2>
+                          <h2 class="mt-5">Let's Get In Touch!</h2>
                           <hr class="divider my-4" />
-                          <p class="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                          <p class="text-muted mb-5">If you have any enquiries for me please do not hesitate to get in touch!</p>
                       </div>
                   </div>
                   <div class="row">
                       <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
                           <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
-                          <div>+1 (555) 123-4567</div>
+                          <div>+44 745-676-4403</div>
                       </div>
                       <div class="col-lg-4 mr-auto text-center">
                           <i class="fas fa-envelope fa-3x mb-3 text-muted"></i
-                          ><!-- Make sure to change the email address in BOTH the anchor text and the link target below!--><a class="d-block" href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a>
+                          ><!-- Make sure to change the email address in BOTH the anchor text and the link target below!--><a class="d-block" href="ofaruk0169@gmail.com">ofaruk0169@gmail.com</a>
                       </div>
                   </div>
               </div>
