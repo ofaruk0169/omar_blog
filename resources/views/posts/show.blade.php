6f8c45a64b9cf0dcfabee4a9e5355f1a1681ab30
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-04-26 13:38:12 +0100

    Changed teh show layouts lightly

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,16 +3,18 @@
 @section('content')
     
     
-    <h1 class="page-section bg-primary" id="blogtitle">{{$post->title}}</h1>
+    <h1 class="page-section " id="blogtitle">{{$post->title}}</h1>
     <div class="row">
         <div class="col-md-12">
             <img style="width: 100%" src="/storage/cover_images/{{$post->cover_image}}" alt="current cover image" class="showimg">
         </div>
     </div>
-    <p class="postbody page-section bg-primary">{{$post->body}} </p>
+    <p class="postbody page-section">{{$post->body}} </p>
     <hr>
 
     <small>Written on {{$post->created_at->format('M d, Y')}} </small>
+
+   
     
     <hr>
     
