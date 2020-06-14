99e17c4b63a5ce924f7d05167b08270e41181b77
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-06-14 02:22:40 +0100

    yeye

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,8 +9,9 @@
                 
             <div class="row">
                 <div class="col-md-6 col-lg-2">
-                <img style="width: 100%" src="/img/finalfinal.png" alt="Blog Image">
+                <img style="width: 100%; text-align: center;" src="/img/finalfinal.png" alt="Blog Image">
                 </div>
+                <br>
                 <div class="col-md-8 col-lg-8">
                         <h1 class="blogh1"><a href="/posts/{{$post->id}}">{{ $post->title  }}</a></h1>
                        
